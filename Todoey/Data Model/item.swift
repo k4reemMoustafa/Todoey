9af05076b3da4efe610f0c38000f49dd2d412cf0
Kareem <karim_mustafa@icloud.com>
2018-11-09 22:30:35 +0300

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Kareem on 08/11/2018.
+//  Copyright © 2018 Kareem. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
